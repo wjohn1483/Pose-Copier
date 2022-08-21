0d61d7924479177d8df0f216696ca99150a9ddf6
wjohn1483 <wjohn1483@yahoo.com.tw>
2022-08-21 17:33:08 +0800

    Add selection of image

---
+++ src/image.js
@@ -0,0 +1,10 @@
+export function showImage(src, target) {
+  var fr=new FileReader();
+  // when image is loaded, set the src of the image where you want to display it
+  fr.onload = function(e) { target.src = this.result; };
+  src.addEventListener("change",function() {
+    // fill fr with image data    
+    fr.readAsDataURL(src.files[0]);
+  });
+}
+
